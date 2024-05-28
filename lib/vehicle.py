28f74afd7dd903c2490e9105567027e106952258
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-05-28 07:22:32 +0300

    finish task

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,10 @@
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheen_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheen_number
+
+    def go(self):
+        return "vrrrrrrrooom!"
+    
+    def fill_up_tank(self):
+        return "filling up!"
